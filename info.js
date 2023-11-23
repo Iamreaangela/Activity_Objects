5b0f0232190a70991c713847d820daf8ecae73d6
Rea <gallerosrea45@gmail.com>
2023-11-23 22:03:21 +0800

    MobApp
    Activity_4

---
+++ info.js
@@ -0,0 +1,103 @@
+export const users = [
+    {
+      nickName:'Bado',
+      name: {
+        firstName:'REA ANGELA',
+        lastName:'GALLEROS',
+      },
+      course:'BSIT',
+      year:'3',
+      
+    },
+    {
+      nickName:'Mayang',
+      name: {
+        firstName:'REA',
+        lastName:'ACORDA',
+      },
+      course:'BSEED',
+      year:'1',
+    },
+    {
+      nickName:'Tapil',
+      name: {
+        firstName:'ANAPHIL',
+        lastName:'JAOJAO',
+      },
+      course:'HM',
+      year:'1',
+    },
+    {
+      nickName:'Tony',
+      name: {
+        firstName:'ANTHONY',
+        lastName:'AMPASIN',
+      },
+      course:'SEAMAN',
+      year:'3',
+    },
+    {
+      nickName:'Long-long',
+      name: {
+        firstName:'MARK ANTONY',
+        lastName:'JANDUGAN',
+      },
+      course:'BSCS',
+      year:'2',
+    },
+    {
+        nickName:'Epli',
+        name: {
+          firstName:'JEFREY',
+          lastName:'CASILA',
+        },
+        course:'BSET',
+        year:'2',
+      },
+      {
+        nickName:'Mitchay',
+        name: {
+          firstName:'MITCH',
+          lastName:'IGCALINOS',
+        },
+        course:'HM',
+        year:'2',
+      },
+      {
+        nickName:'Kimmy',
+        name: {
+          firstName:'KIM',
+          lastName:'TAGAYONG',
+        },
+        course:'AGRICULTURE',
+        year:'2',
+      },
+      {
+        nickName:'Mekus',
+        name: {
+          firstName:'INSAN',
+          lastName:'TORNADO',
+        },
+        course:'HM',
+        year:'2',
+      },
+      {
+        nickName:'Baby',
+        name: {
+          firstName:'BABBY',
+          lastName:'GOO',
+        },
+        course:'PILOT',
+        year:'4',
+      },
+      {
+        nickName:'Fjm me amore',
+        name: {
+          firstName:'FELISE JEANNE MARIE',
+          lastName:'VALE',
+        },
+        course:'MEDTECH',
+        year:'4',
+      },
+      
+  ];
